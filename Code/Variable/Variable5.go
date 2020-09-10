9bf5f78056eab46a5b59ba72eea751ef2a1c4521
siahk <LingersFA@outlook.com>
2020-09-10 18:01:14 +0800

    0910

--- Code/Variable/Variable5.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	f := "Runoob" // var f string = "Runoob"
-
-	fmt.Println(f)
-}
--- Code/Variable/Variable7.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var a string = "abc"
-	fmt.Println("hello, world", a)
-}
